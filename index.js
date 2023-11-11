223ae579145e29f61831fbe63834bc6a1c94735f
Faizan2310 <210057917@aston.ac.uk>
2023-11-11 15:32:34 +0000

    button1 functionality added

--- index.js
+++ index.js
@@ -1,15 +1,34 @@
 const player = document.querySelector("#player");
 const computer = document.querySelector("#computer");
+const message = document.querySelector("#message");
 const rockBtn = document.querySelector("#rockBtn");
 const paperBtn = document.querySelector("#paperBtn");
 const scissorsBtn = document.querySelector("#scissorsBtn");
+const container = document.querySelector("#container");
 
 
 
 rockBtn.addEventListener("click",()=>{
-    player.textContent = "Player: Rock"
-    computer.textContent = "Computer:" + getRandom(arr);
+    const computerChoice = getRandom(arr);
+    player.textContent = "Rock";
+    computer.textContent = computerChoice;
     
+    switch (computerChoice) {
+        case "Rock":
+            message.textContent = "Draw";
+            break;
+        case "Paper":
+            message.textContent = "You Lost!";
+            break;
+        case "Scissors":
+            message.textContent = "You Won!";
+            player.classList.add('celebrate');
+            setTimeout(() => {
+                player.classList.remove('celebrate');
+            }, 1000);
+            break;
+
+    }
 })
 
 
